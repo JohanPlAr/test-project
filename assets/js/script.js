97a15a94fea33c3a6a051ce0c74b138e00631454
Johan Plym Arkert <johan.arkert@gmail.com>
2023-08-14 15:55:33 +0200

    Add functions for score etc

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,40 @@ function runGame() {
 
 let turn = 0;
 let gameArrays = [[], [], [], [], [], [], []];
+let gameColumns = document.getElementsByClassName("game-column");
+let p1Score = 0;
+let p2Score = 0;
 
 function markColumn() {
-  let gameColumns = document.querySelectorAll(".game-column");
-  gameColumns.forEach(function (div) {
-    div.addEventListener("mouseover", function () {
-      div.style.backgroundColor = "green";
-    });
-    div.addEventListener("mouseout", function () {
-      div.style.backgroundColor = "initial";
+  let markedColumns = document.querySelectorAll(".game-column");
+  markedColumns.forEach(function (column) {
+    column.addEventListener("mouseover", function () {
+      column.style.backgroundColor = "green";
+         });
+    column.addEventListener("mouseout", function () {
+      column.style.backgroundColor = "initial";
     });
   });
 }
+function markPosition(){
+    let gameBoxes = document.querySelectorAll(".game-box");
+     
+    gameBoxes.forEach(function (box) {
+        if (box.style.backgroundColor == "white"){
+        box.addEventListener("mouseover", function (){
+           
+                            box.style.backgroundColor = "green";
+        
+        });
+        }
+box.addEventListener("mouseout", function () {
+ if (box.style.backgroundColor == "green"){
+    box.style.backgroundColor = "white";}
+});   
+});    
+}
 
 function choseColumn() {
-  let gameColumns = document.getElementsByClassName("game-column");
   let playerColumn = 0;
   for (let i = 0; i < gameColumns.length; i++)
     gameColumns[i].addEventListener("click", function () {
@@ -25,8 +44,16 @@ function choseColumn() {
       console.log(turn);
       if (turn % 2 === 0) {
         addCoin(playerColumn, "red");
+        document.getElementById("p1-turn").style.backgroundColor = "white";
+        document.getElementById("p2-turn").style.backgroundColor = "yellow";
+        document.getElementById("p2-turn-text").innerText = "Next play";
+        document.getElementById("p1-turn-text").innerText = "";
       } else {
         addCoin(playerColumn, "yellow");
+        document.getElementById("p2-turn").style.backgroundColor = "white";
+        document.getElementById("p1-turn").style.backgroundColor = "red";
+        document.getElementById("p1-turn-text").innerText = "Next play";
+        document.getElementById("p2-turn-text").innerText = "";
       }
       turn += 1;
     });
@@ -34,31 +61,35 @@ function choseColumn() {
 
 function checkWinner(color) {
     for (let i = 0; i < gameArrays.length; i++) {
-        for (let j = 0; j < gameArrays[i].length; j++){ 
+        for (let j = 0; j < gameArrays[i].length; j++){
+            
             if (i<4 && gameArrays[i][j]== color && gameArrays[i+1][j]== color && gameArrays[i+2][j]== color && gameArrays[i+3][j]== color){
                 console.log("You win");
                 document.getElementById("winner-text").innerText = `${color} Wins!`;
-                
+                gameScore(color);
+                      
             }
             if (gameArrays[i][j]== color && gameArrays[i][j+1]== color && gameArrays[i][j+2]== color && gameArrays[i][j+3]== color){
                 console.log("You win");
                 document.getElementById("winner-text").innerText = `${color} Wins!`;
+                gameScore(color);
                 
             }
             if (i<4 && gameArrays[i][j]== color && gameArrays[i+1][j+1]== color && gameArrays[i+2][j+2]== color && gameArrays[i+3][j+3]== color){
                 console.log("You win");
                 document.getElementById("winner-text").innerText = `${color} Wins!`;
+                gameScore(color);
                 
             }
             if (i>2 && gameArrays[i][j]== color && gameArrays[i-1][j-1]== color && gameArrays[i-2][j-2]== color && gameArrays[i-3][j-3]== color){
                 console.log("You win");
                 document.getElementById("winner-text").innerText = `${color} Wins!`;
-                
+                gameScore(color);
             }
             if (i>2 && gameArrays[i][j]== color && gameArrays[i-1][j+1]== color && gameArrays[i-2][j+2]== color && gameArrays[i-3][j+3]== color){
                 console.log("You win");
                 document.getElementById("winner-text").innerText = `${color} Wins!`;
-                
+                gameScore(color);
             }
 
 }
@@ -78,7 +109,14 @@ function displayCoins(color) {
   }
 }
 
-function gameScore() {}
+function gameScore(color) {
+if (color == "red"){
+    p1Score += 1;
+    document.getElementById("p1-score").innerText = `Score ${p1Score}`;    }
+if (color == "yellow"){p2Score += 1
+    document.getElementById("p2-score").innerText = `Score ${p2Score}`;   }
+console.log(p1Score, p2Score)
+}
 
 function addCoin(playerColumn, color) {
   if (gameArrays[playerColumn].length < 6) {
@@ -89,8 +127,35 @@ function addCoin(playerColumn, color) {
   checkWinner(color);
 }
 
-markColumn();
-choseColumn();
+function resetBoard(){ 
+       
+    for (let i = 0; i < gameArrays.length; i++) {
+        for (let j = 0; j < gameArrays[i].length; j++) {
+            document.getElementById(`x${j + 1}, y${i + 1}`).style.backgroundColor = "white";
+          
+  
+}
+}
+gameArrays = [[], [], [], [], [], [], []];
+document.getElementById("winner-text").innerText = "";
+
+}
+function resetScore(){
+    let p1Score = 0;
+    let p2Score = 0;
+    document.getElementById("p1-score").innerText = `Score ${p1Score}`;
+    document.getElementById("p2-score").innerText = `Score ${p2Score}`;
 }
 
-runGame()
+markColumn();
+markPosition();
+choseColumn();
+let resetBoardButton = document.getElementById("reset-board-button");
+resetBoardButton.addEventListener("click", resetBoard);
+let resetScoreButton = document.getElementById("reset-score-button");
+resetScoreButton.addEventListener("click", resetScore)
+}
+
+let startButton = document.getElementById("start-button");
+startButton.addEventListener("click", runGame);
+
