7078d52c73089836c79e68ab082f7f79800b780f
Johan Plym Arkert <johan.arkert@gmail.com>
2023-08-17 14:32:35 +0200

    add draw

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,13 +20,30 @@ rulesModal.style.display = "none";
 
 function runGame() {
 
-let turn = 0;
-let gameArrays = [[], [], [], [], [], [], []];
-let gameColumns = document.getElementsByClassName("game-column");
-let p1Score = 0;
-let p2Score = 0;
-document.getElementById("p1-turn").style.backgroundColor = "red";
-document.getElementById("p1-turn-text").innerText = "Next Play";
+  let gameArrays = [[], [], [], [], [], [], []];
+  let turn = 0;
+  let gameColumns = document.getElementsByClassName("game-column");
+  let p1Score = 0;
+  let p2Score = 0;
+  
+  function resetGame(){
+    p1Score = 0;
+    p2Score = 0;
+    document.getElementById("p1-turn").style.backgroundColor = "red";
+    document.getElementById("p1-turn-text").innerText = "Next Play";
+    document.getElementById("p2-turn").style.backgroundColor = "";
+    document.getElementById("p2-turn-text").innerText = "";
+    let boxes = document.querySelectorAll('.game-box');
+    boxes.forEach(function(box) {
+       box.classList.remove('active');
+       box.style.backgroundColor = "white";
+    });
+    
+  }
+    resetGame()
+
+
+
 
 
 
@@ -34,7 +51,7 @@ function markColumn() {
   let markedColumns = document.querySelectorAll(".game-column");
   markedColumns.forEach(function (column) {
     column.addEventListener("mouseover", function () {
-      column.style.backgroundColor = "light-blue";
+      column.style.backgroundColor = "";
       let boxes = Array.from(column.querySelectorAll(".game-box"));
       boxes = boxes.filter((x) => !x.classList.contains("active"));
       console.log(boxes);
@@ -74,11 +91,17 @@ function choseColumn() {
         document.getElementById("p1-turn-text").innerText = "Next Play";
         document.getElementById("p2-turn-text").innerText = "";
       }
-      turn += 1;
+            
     });
   }
 
 function checkWinner(color) {
+if (turn > 41) {
+  turn = 0
+  let draw = true
+  afterWinMenu(color, draw)
+}  
+ 
     for (let i = 0; i < gameArrays.length; i++) {
         for (let j = 0; j < gameArrays[i].length; j++){
             
@@ -86,18 +109,16 @@ function checkWinner(color) {
                  gameArrays[i+1][j]== color &&
                   gameArrays[i+2][j]== color &&
                    gameArrays[i+3][j]== color){
-                console.log("You win");
+                console.log(`${color} wins`);
                 gameScore(color);
                 afterWinMenu(color);
-             
-              
-                     
+                                               
             }
             if (gameArrays[i][j]== color &&
                  gameArrays[i][j+1]== color &&
                   gameArrays[i][j+2]== color &&
                    gameArrays[i][j+3]== color){
-                console.log("You win");
+                console.log(`${color} wins`);
                 gameScore(color);
                 afterWinMenu(color);
          
@@ -106,7 +127,7 @@ function checkWinner(color) {
                  gameArrays[i+1][j+1]== color &&
                   gameArrays[i+2][j+2]== color &&
                    gameArrays[i+3][j+3]== color){
-                console.log("You win");
+                console.log(`${color} wins`);
                 gameScore(color);
                 afterWinMenu(color);
                
@@ -115,7 +136,7 @@ function checkWinner(color) {
                  gameArrays[i-1][j-1]== color &&
                   gameArrays[i-2][j-2]== color &&
                    gameArrays[i-3][j-3]== color){
-                console.log("You win");
+                console.log(`${color} wins`);
                 gameScore(color);
                 afterWinMenu(color);
                
@@ -124,11 +145,11 @@ function checkWinner(color) {
                 gameArrays[i-1][j+1]== color && 
                 gameArrays[i-2][j+2]== color && 
                 gameArrays[i-3][j+3]== color){
-                console.log("You win");
+                console.log(`${color} wins`);
                 gameScore(color);
                 afterWinMenu(color)               
             }
-
+           
 }
 
 }
@@ -157,6 +178,7 @@ console.log(p1Score, p2Score)
 }
 
 function addCoin(playerColumn, color) {
+  turn += 1;
   if (gameArrays[playerColumn].length < 6) {
     gameArrays[playerColumn].push(color);
   }
@@ -169,18 +191,11 @@ function resetBoard(){
   let boxes = document.querySelectorAll('.game-box');
   boxes.forEach(function(box) {
      box.classList.remove('active');
-      });
-    for (let i = 0; i < gameArrays.length; i++) {
-        for (let j = 0; j < gameArrays[i].length; j++) {
-            document.getElementById(`x${j + 1}, y${i + 1}`).style.backgroundColor = "white";
-          
-          
-}
-}
+     box.style.backgroundColor = "white";
+    });
 gameArrays = [[], [], [], [], [], [], []];
-
-
 }
+
 function resetScore(){
     let p1Score = 0;
     let p2Score = 0;
@@ -188,16 +203,23 @@ function resetScore(){
     document.getElementById("p2-score").innerText = `Score ${p2Score}`;
 }
 
-function afterWinMenu(color){   
-resetBoard()  
+function afterWinMenu(color, draw){ 
+resetBoard()
+if (turn > 41) {
+  turn = 0}
 afterWinModal.style.display = "flex";
 let span = document.getElementsByClassName("close")[1];
 span.onclick = function() {
 afterWinModal.style.display = "none";
 }
-document.getElementById("winner-text").innerText = `${color} Wins!`;
+
+if (draw == true){
+  document.getElementById("winner-text").innerText = `It's a draw`;
+}
+else {
+  document.getElementById("winner-text").innerText = `${color} Wins!`;
+  }
 newRound();
-quitGame();
 }
 
 markColumn();
@@ -213,32 +235,23 @@ function newRound(){
   newRoundButton.onclick = function() {
     afterWinModal.style.display = "none";
   }
+}
+function quitGame(){window.location.reload()} 
 
-}
-function quitGame(){
-  let quitGameButton = document.getElementById("quit-game-button");
-  if (quitGameButton.onClick) {window.location.reload()}
-  
-}
+let quitGameButton = document.getElementById("quit-game-button");
+quitGameButton.addEventListener("click", quitGame);
 }
 
 let clickCount = 0;
-
+let startButton = document.getElementById("start-button");
 function handleClick() {
-  let resetBoard = runGame().resetBoard;
   clickCount++;
-  if (clickCount >1) {
+  if (clickCount > 1) {
     clickCount = 0;
-    alert("Warning! Clicking on the 'Start Game'-button again will reset the score and erase board and the game will start from the beginning")
-    startButton.addEventListener("click", function(){
-      resetBoard();
-      
-      
-    });
-  
-  }
+    alert("Warning! Clicking the 'Play Game'-button again will reset the score, erase board and the game will start from the beginning")   
+    }
+   
   else {runGame()}
 }
-let startButton = document.getElementById("start-button");
 startButton.addEventListener("click", handleClick);
-  
+
